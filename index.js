2caab8ee10e526906cd0e3152ce416acbeb093eb
Kamil Rzeszutek <kamilrzeszutek@Kamils-MacBook-Pro.local>
2020-01-08 21:59:06 +0100

    v.0004

--- index.js
+++ index.js
@@ -1,20 +1,40 @@
 var notes = [];
+
+
 document.querySelector('.addNote').addEventListener('click', function(){
     console.log('Funkcja klikaj')
-    basicPopup();   
+    basicPopup(); 
+    showNotes();  
 })
+
 if(localStorage.getItem('notePocket') === null){
     localStorage.setItem('notePocket', JSON.stringify(notes));
 }
 
+
+
+document.getElementById('popup').style.display = 'none'
+
 function basicPopup() {
-    popupWindow = window.open("../popup.html",'popUpWindow','height=300,width=500,left=600,top=300,resizable=no,scrollbars=no,toolbar=no,menubar=no,location=no,directories=no, status=yes');
+    let mainStream = document.getElementById('mainStream');
+    let popup = document.getElementById('popup')
+    
+    if (popup.style.display === "none"){
+        mainStream.style.height = "25vw"
+        popup.style.display = 'block'
+    }else {
+        mainStream.style.height = "5vw"
+        popup.style.display = 'none'
+    
+    }
+
+    //popupWindow = window.open("../popup.html",'popUpWindow','height=300,width=500,left=600,top=300,resizable=no,scrollbars=no,toolbar=no,menubar=no,location=no,directories=no, status=yes');
 };
  document.addEventListener( "DOMContentLoaded", function()  {      // Nasłuchiwanie na wypełnienie formularzu
      var form = document.getElementById( "form" );
      form.addEventListener( "submit", function(e) {
          e.preventDefault();
-         var json = toJSONString(this);
+         let json = JSON.stringify(this)
      }, false);
  });
 
@@ -23,17 +43,47 @@ function basicPopup() {
 
 
  function showNotes (){
-     var note = document.querySelector('#notes')
+     var note = document.getElementById('notes')
      for (let i = 1; i<note.childElementCount; i++)
      {
          note.removeChild[i]
      }
+     
+     notes.forEach(e =>{
+        createNote()
+
+        if(e.priority === 1){ //priorytet
+            note.style.order = 1;
+        }
+            TODO //OPIS 
+    })
+
+
+
  }
 
- function createNote(){
-     var noteId = 0
+ var noteId = 0
+
+ function createNote(){  //tworzy notatke
      notes.forEach(e, function(){
         let newNote = document.createElement('div');
-        newNote.className = "note" //TODO dodac kolor
+        newNote.className = "note" // TODO dodac kolor
+        note.id=noteId++
      })
  }
+function addTextBoxes(){
+    let noteTitle = document.createElement('textarea');
+    noteTitle.className = 'noteTitle';
+    noteTitle.value = e.title;
+    noteTitle.disabled = true;
+    
+    let noteDescription = document.createElement('textarea');
+    noteDescription.className = 'noteDescription';
+    noteDescription.value = e.description;
+    noteDescription.disabled = true;
+
+    let noteFooter = document.createElement('textarea');
+    noteFooter.className = 'noteFooter';
+    noteFooter.value = e.description;
+    noteFooter.appendChild(document.createTextNode("Created: " + e.date))
+}
